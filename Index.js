3b0740b46a7908e8bf609b6ab9fe4a941fd60cc5
Rachel Wagschal <rawagschal@gmail.com>
2021-01-30 15:15:48 -0800

    initial createManager() function that starts the app

--- Index.js
+++ Index.js
@@ -6,14 +6,13 @@ const Manager = require('./lib/Manager');
 const inquirer = require('inquirer');
 const path = require("path");
 const fs = require("fs");
-const { validate } = require('@babel/types');
-const { getConsoleOutput } = require('@jest/console');
 
 // output files here
 
 // to hold team members
 var teamMembers = [];
 
+// questions for all employee types
 const baseQuestions = [
     {
         type: 'input',
@@ -44,15 +43,98 @@ const baseQuestions = [
 
 ];
 
-const init = () => {
-    return inquirer.prompt([
+// initial function that requires manager input
+function createManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Please enter your team manager's first and last name:",
+                validate: managerNameInput => {
+                    if (managerNameInput) {
+                        return true;
+                    } else {
+                        console.log("You must enter your team manager's first and last name!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please assign your team manager an ID number:",
+                validate: managerIdInput => {
+                    if (managerIdInput) {
+                        return true;
+                    } else {
+                        console.log("You must assign your team manager an ID number!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please enter your team manager's email address:",
+                validate: managerEmailInput => {
+                    if (managerEmailInput) {
+                        return true;
+                    } else {
+                        console.log("You must enter your team manager's email address!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "Please enter your team manager's office number:",
+                validate: managerOfficeNumberInput => {
+                    if (managerOfficeNumberInput) {
+                        return true;
+                    } else {
+                        console.log("You must enter your team manager's office number!");
+                        return false;
+                    }
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            addEmployee();
+        });
+    }
+createManager();
+// console.log(teamMembers);
+
+
+function addEmployee() {
+    inquirer.prompt([
         {
-            type: 'list',
-            name: 'initialOptions',
-            message: 'Choose which action you would like to perform',
-            choices: ['Add Manager', 'Add Engineer', 'Add Intern']
+            type: "list",
+            name: "menuOption",
+            message: "What would you like to do next?",
+            choices: [
+                "Add an engineer",
+                "Add an intern",
+                "Finish building my team"
+            ]
         }
-    ])
+    ]).then(chosenOption => {
+        switch (chosenOption.menuOption) {
+            case "Add an engineer":
+                addEngineer();
+                break;
+            case "Add an intern":
+                addIntern();
+                break;
+            case "Finish building my team":
+                finishTeam();
+        }
+    });
 }
 
-init();
+function finishTeam() {
+    
+}
+
